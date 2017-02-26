14acf38c0196ecf8f4cddbe018f0ba8e066d5a27
Paweł Tomasik <pawel.tomasik@pictec.eu>
2017-02-26 02:41:14 +0100

    Settings file and gitignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import signal
 import sys
 
@@ -8,27 +9,43 @@ USAGE="""
 Botnet instance.
 
 Usage:
-  python main.py client|server [-r command]
+  python main.py client|server|hybrid [-r command]
 
 Options:
   -r   Execute a command and quit
 
+ToDo:
+  -p   Run on specific port
+
 """
 
 MAINPR = processor.MainProcessor
+SETFILE = "settings.json"
 
 if __name__ == '__main__':
     try:
+        settings = json.load(SETFILE)
         if sys.argv[1]=='client':
-            sole_actor = actor.Actor(targetProcessor=MAINPR)
+            sole_actor = actor.Actor(targetProcessor=MAINPR,
+                         threads=settings['clientThreads'],
+                         bind_ip=settings['clientIP'],
+                         bind_port=settings['clientPort'])
             signal.pause()
         elif sys.argv[1]=='server':
             if '-r' in sys.argv[2:]:
                 command = ' '.join(sys.argv[(sys.argv.index('-r')+1):])
-                console = actor.Console(targetProcessor=MAINPR,cmd=command)
+                console = actor.Console(targetProcessor=MAINPR,cmd=command,
+                          threads=settings['serverThreads'],
+                          bind_ip=settings['serverIP'],
+                          bind_port=settings['serverPort'])
             else:
-                console = actor.Console(targetProcessor=MAINPR)
-            signal.pause()
+                console = actor.Console(targetProcessor=MAINPR,
+                          threads=settings['serverThreads'],
+                          bind_ip=settings['serverIP'],
+                          bind_port=settings['serverPort'])
+                signal.pause()
+        elif sys.argv[1]=='hybrid':
+            raise NotImplementedError
         else:
             print "Please choose either client or server functionality"
     except IndexError:
