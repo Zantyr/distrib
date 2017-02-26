ca59b713f27c42fbbc88cf80cfde322c3805987b
Paweł Tomasik <pawel.tomasik@pictec.eu>
2017-02-26 02:18:40 +0100

    Ordering the changes

--- processor.py
+++ processor.py
@@ -1,4 +1,44 @@
-class test(processor):
+class Processor(object):
+    def __init__(self,inbox,outbox):
+        """
+        This object processes messages. Looks into inbox, picks a msg,
+        and returns processed msg into the outbox. Has internal state, which
+        dictates what messages to look.
+        """
+        self.inbox = inbox
+        self.outbox = outbox
+        self.thread = threading.Thread(target=self.execute)
+    def execute(self):
+        """
+        Method passed to thread after its instantiation. Does the real processing
+        """
+        raise NotImplementedError
+
+    def run(self):
+        self.thread.start()
+        
+    def interrupt(self,signal):
+        """
+        Interrupts the processor, may cancel or suspend transaction.
+        Is superior to any other computation on the processor.
+        Interrupts are queuable
+        """
+        raise NotImplementedError
+
+
+class MockProcessor(Processor):
+    def execute(self):
+        while True:
+            try:
+                item = self.inbox.popleft()
+                print "{} received: {}".format(id(self),item)
+            except IndexError:
+                pass
+
+
+
+
+class MainProcessor(Processor):
     def execute(self):
         while True:
             try:
